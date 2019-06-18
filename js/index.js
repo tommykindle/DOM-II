c2c9df995e6e37a88032fcf16f4b7b6c5526c064
Tommy <tommykindle83@gmail.com>
2019-06-18 14:25:23 -0700

    Nav links change on over and time out Heading changes on click and dbl click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const nav = document.querySelector('header nav')
+nav.addEventListener('click', event =>{
+  const heading = document.querySelector('.logo-heading')
+  heading.textContent = event.target.textContent
+}); 
+
+const navColor = document.querySelector('header nav')
+navColor.addEventListener('mouseover', event => {
+    event.target.style.color = 'green'; 
+    setTimeout(function(){
+        event.target.style.color = "black"; 
+    },1000); 
+}, false); 
+
+const headingReset = document.querySelector('.logo-heading')
+headingReset.addEventListener('dblclick', event =>{
+    headingReset.textContent = 'Fun Bus'
+}); 
